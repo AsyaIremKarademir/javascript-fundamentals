9a90b3ca15092eb47c9ed7a8078ca5cf60b9021f
Asya Irem Karademir <a.iremkarademir@gmail.com>
2026-07-21 14:23:13 +0300

    variables

---
+++ ders2/index.js
@@ -0,0 +1,33 @@
+
+// let x;
+// x=123;
+// console.log(x); 
+
+
+// let age = 22;
+// console.log(`My age is ${age} years old.`);
+// console.log(typeof age);
+
+// let age1=22.5;
+// console.log(`Her age is ${age1} years old.`);
+// console.log(typeof age1);
+
+// let firstName = "Asya";
+// console.log(`Her name is ${firstName}.`);
+// console.log(typeof firstName);
+
+// let online = true;
+// console.log(`Is she online? ${online}.`);
+// console.log(typeof online);
+
+// let forSale= true;
+// console.log(`Is it for sale? ${forSale}.`);
+// console.log(typeof forSale);
+
+let fullName = "Asya İrem";
+let age = 22;
+let student = true;
+
+document.getElementById("p1").textContent = fullName;
+document.getElementById("p2").textContent = age;
+document.getElementById("p3").textContent = student;
