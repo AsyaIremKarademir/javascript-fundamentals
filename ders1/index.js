ccbda49c6aeeb19856db20cc5c41e74c2f6685f0
Asya Irem Karademir <a.iremkarademir@gmail.com>
2026-07-21 11:44:53 +0300

    ilk ders

---
+++ ders1/index.js
@@ -0,0 +1,5 @@
+console.log("Hello, World!");
+console.log(" I am learning JavaScript.");
+
+document.getElementById("myH1").textContent="Welcome to My Website";
+document.getElementById("myP").textContent="This is a simple website created using HTML, CSS, and JavaScript.";
