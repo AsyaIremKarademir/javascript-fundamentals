2071164884b6cae3afda54570f2667961a578ac8
Asya Irem Karademir <a.iremkarademir@gmail.com>
2026-07-23 11:53:24 +0300

    ternary operatoru

---
+++ ders8/index.js
@@ -0,0 +1,10 @@
+//ternary operatörü
+
+// let age=22;
+// let result=age>=18 ? "Reşitsiniz" : "Reşit değilsiniz";
+// console.log(result);
+
+
+let tutar=1000;
+let indirim=tutar>=700 ? 10 : 0;
+console.log(`your total is $${tutar - tutar *(indirim/100)}`);
