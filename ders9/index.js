56f6222ffff04e1da51de0ab9a6b16a5b11b343d
Asya Irem Karademir <a.iremkarademir@gmail.com>
2026-07-23 12:06:25 +0300

    switches

---
+++ ders9/index.js
@@ -0,0 +1,31 @@
+//switches kullanımı
+
+let day=1;
+
+switch(day){
+    case 1:
+        console.log("Pazartesi");
+        break;
+    case 2:
+        console.log("Salı");
+        break;
+    case 3:
+        console.log("Çarşamba");
+        break;
+        case 4:
+        console.log("Perşembe");
+        break;
+    case 5:
+        console.log("Cuma");
+        break;
+    case 6:
+        console.log("Cumartesi");
+        break;
+    case 7:
+        console.log("Pazar");
+        break;
+    default:
+        console.log("Geçersiz gün");
+}
+
+//çok fazla if else kullanmak yerine switch case kullanmak daha mantıklıdır.
