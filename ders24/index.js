5d5f931060b90ce95976aafa7e71d56258e89efa
Asya Irem Karademir <a.iremkarademir@gmail.com>
2026-07-30 11:11:02 +0300

    filter method

---
+++ ders24/index.js
@@ -0,0 +1,4 @@
+//filter()=adından da anlaşılacağı gibi bir dizideki elemanları belirlediğiniz bir koşula göre filtreler.
+//Her elemanı test eder; koşulu sağlayanları (true dönenleri) tutar, sağlamayanları çöpe atar.
+//ve sadece geçenlerden oluşan yeni bir dizi döndürür.
+
